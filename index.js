c0b088a11cc522b5acba77b135492cf668f4b7d5
Vincent Claeys - Esprits Libres <vincent.claeys@espritslibres.ca>
2016-09-13 15:55:40 -0400

    Fixed for ES5

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 module.exports = function loadImageAsDataURL(imgURI, cb){
-  const img = document.createElement('img')
-  const canvas = document.createElement('canvas')
+  var img = document.createElement('img');
+  var canvas = document.createElement('canvas');
 
   // Required for GMaps and certain sites
   // read more on: http://stackoverflow.com/a/20424457/2374061
-  img.setAttribute('crossOrigin', 'anonymous')
-  img.onload = () => {
-    const ctx = canvas.getContext('2d')
+  img.setAttribute('crossOrigin', 'anonymous');
+  img.onload = function(){
+    var ctx = canvas.getContext('2d');
 
-    canvas.height = img.naturalHeight
-    canvas.width = img.naturalWidth
+    canvas.height = img.naturalHeight;
+    canvas.width = img.naturalWidth;
 
-    ctx.drawImage(img, 0, 0)
-    cb(null, canvas.toDataURL('image/png'))
+    ctx.drawImage(img, 0, 0);
+    cb(null, canvas.toDataURL('image/png'));
   }
 
-  img.onerror = e => {
-    cb(e, null)
+  img.onerror = function(error){
+    cb(e, null);
   }
 
-  img.src = imgURI
+  img.src = imgURI;
 }
